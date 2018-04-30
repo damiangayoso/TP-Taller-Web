5bd87dcc6d6b3bdf4d7b3c30a0801b613290577c
tomasjg <tomas.1412@gmail.com>
2018-04-30 19:21:24 -0300

    Add del Test que busca farmacias por calle
    Con input de calle a buscar

---
+++ proyecto-tp/src/test/java/ar/edu/unlam/tallerweb1/BuscarFarmaciaPorCalleTest.java
@@ -0,0 +1,51 @@
+package ar.edu.unlam.tallerweb1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import ar.edu.unlam.tallerweb1.modelo.Direccion;
+import ar.edu.unlam.tallerweb1.modelo.Farmacia;
+
+public class BuscarFarmaciaPorCalleTest extends SpringTest{
+
+	@Test
+    @Transactional @Rollback(true)
+    public void buscarPorDireccion(){
+		String calleBusqueda;
+		
+		Direccion direccion = new Direccion();
+		direccion.setCalle("Prueba Calle");
+		direccion.setNumero(4568);
+		
+		Session session = getSession();
+    	session.save(direccion);
+    	
+    	Farmacia farmacia = new Farmacia();
+    	farmacia.setNombre("Farmacia A");
+    	farmacia.setTelefono("4568-9874");
+    	farmacia.setDireccion(direccion);
+    	session.save(farmacia);
+    	
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Escriba la Caller a Buscar:");
+    	calleBusqueda = sc.nextLine();
+    	
+    	List<Farmacia> resultado = getSession().createCriteria(Farmacia.class)
+    			.createAlias("direccion", "calleBuscada")
+    			.add(Restrictions.eq("calleBuscada.calle", calleBusqueda))
+    			.list();
+    	
+    	assertThat(resultado).isNotEmpty();
+    	for(Farmacia calleResultado: resultado){
+        	assertThat(calleResultado.getDireccion().getCalle()).isEqualTo("Prueba Calle");       	
+    	}
+    	
+	}
+}
